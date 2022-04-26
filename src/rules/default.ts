69fa9063cedcedc61e9d474888fcb538edf48683
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-04-25 22:02:29 -0400

    feat: add object-fit and object-position

--- src/rules/default.ts
+++ src/rules/default.ts
@@ -1,25 +1,56 @@
-import type { Rule } from '@unocss/core'
-import { transitions } from './transition'
-import { borders } from './border'
-import { bgColors, opacity, textColors } from './color'
-import { flex } from './flex'
-import { fonts, tabSizes, textIndents, textShadows, textStrokes } from './typography'
-import { gaps } from './gap'
-import { grids } from './grid'
-import { overflows } from './layout'
-import { alignments, boxSizing, floats, insets, justifies, orders, placements, positions, zIndexes } from './position'
-import { rings } from './ring'
-import { boxShadows } from './shadow'
-import { aspectRatio, sizes } from './size'
-import { margins, paddings } from './spacing'
-import { appearances, breaks, contents, cursors, displays, fontSmoothings, fontStyles, pointerEvents, resizes, textOverflows, textTransforms, userSelects, whitespaces } from './static'
-import { transforms } from './transform'
-import { cssProperty, cssVariables } from './variables'
-import { questionMark } from './question-mark'
-import { textAligns, verticalAligns } from './align'
-import { appearance, outline, willChange } from './behaviors'
-import { textDecorations } from './decoration'
-import { svgUtilities } from './svg'
+import type { Rule } from "@unocss/core";
+import { transitions } from "./transition";
+import { borders } from "./border";
+import { bgColors, opacity, textColors } from "./color";
+import { flex } from "./flex";
+import {
+  fonts,
+  tabSizes,
+  textIndents,
+  textShadows,
+  textStrokes,
+} from "./typography";
+import { gaps } from "./gap";
+import { grids } from "./grid";
+import { overflows } from "./layout";
+import {
+  alignments,
+  boxSizing,
+  floats,
+  insets,
+  justifies,
+  orders,
+  placements,
+  positions,
+  zIndexes,
+} from "./position";
+import { rings } from "./ring";
+import { boxShadows } from "./shadow";
+import { aspectRatio, sizes } from "./size";
+import { margins, paddings } from "./spacing";
+import {
+  appearances,
+  breaks,
+  contents,
+  cursors,
+  displays,
+  fontSmoothings,
+  fontStyles,
+  objectPositions,
+  pointerEvents,
+  resizes,
+  textOverflows,
+  textTransforms,
+  userSelects,
+  whitespaces,
+} from "./static";
+import { transforms } from "./transform";
+import { cssProperty, cssVariables } from "./variables";
+import { questionMark } from "./question-mark";
+import { textAligns, verticalAligns } from "./align";
+import { appearance, outline, willChange } from "./behaviors";
+import { textDecorations } from "./decoration";
+import { svgUtilities } from "./svg";
 
 export const rules: Rule[] = [
   cssVariables,
@@ -44,6 +75,7 @@ export const rules: Rule[] = [
   textColors,
   fontStyles,
   fontSmoothings,
+  objectPositions,
   boxShadows,
   rings,
   flex,
@@ -78,4 +110,4 @@ export const rules: Rule[] = [
 
   // should be the last
   questionMark,
-].flat(1)
+].flat(1);
--- src/rules/static.ts
+++ src/rules/static.ts
@@ -1,91 +1,120 @@
-import type { Rule } from '@unocss/core'
-import { handler as h } from '../utils'
+import type { Rule } from "@unocss/core";
+import { positionMap } from "../utils";
+import { handler as h } from "../utils";
 
-export const varEmpty = 'var(--un-empty,/*!*/ /*!*/)'
+export const varEmpty = "var(--un-empty,/*!*/ /*!*/)";
 
 // display table included on table.ts
 export const displays: Rule[] = [
-  ['inline', { display: 'inline' }],
-  ['block', { display: 'block' }],
-  ['inline-block', { display: 'inline-block' }],
-  ['contents', { display: 'contents' }],
-  ['flow-root', { display: 'flow-root' }],
-  ['list-item', { display: 'list-item' }],
-  ['hidden', { display: 'none' }],
-]
+  ["inline", { display: "inline" }],
+  ["block", { display: "block" }],
+  ["inline-block", { display: "inline-block" }],
+  ["contents", { display: "contents" }],
+  ["flow-root", { display: "flow-root" }],
+  ["list-item", { display: "list-item" }],
+  ["hidden", { display: "none" }],
+];
 
 export const appearances: Rule[] = [
-  ['visible', { visibility: 'visible' }],
-  ['invisible', { visibility: 'hidden' }],
-  ['backface-visible', { 'backface-visibility': 'visible' }],
-  ['backface-hidden', { 'backface-visibility': 'hidden' }],
-]
+  ["visible", { visibility: "visible" }],
+  ["invisible", { visibility: "hidden" }],
+  ["backface-visible", { "backface-visibility": "visible" }],
+  ["backface-hidden", { "backface-visibility": "hidden" }],
+];
 
 export const cursors: Rule[] = [
   [/^cursor-(.+)$/, ([, c]) => ({ cursor: h.bracket.cssvar(c) || c })],
-]
+];
 
 export const pointerEvents: Rule[] = [
-  ['pointer-events-auto', { 'pointer-events': 'auto' }],
-  ['pointer-events-none', { 'pointer-events': 'none' }],
-]
+  ["pointer-events-auto", { "pointer-events": "auto" }],
+  ["pointer-events-none", { "pointer-events": "none" }],
+];
 
 export const resizes: Rule[] = [
-  ['resize-x', { resize: 'horizontal' }],
-  ['resize-y', { resize: 'vertical' }],
-  ['resize', { resize: 'both' }],
-  ['resize-none', { resize: 'none' }],
-]
+  ["resize-x", { resize: "horizontal" }],
+  ["resize-y", { resize: "vertical" }],
+  ["resize", { resize: "both" }],
+  ["resize-none", { resize: "none" }],
+];
 
 export const userSelects: Rule[] = [
-  ['select-auto', { 'user-select': 'auto' }],
-  ['select-all', { 'user-select': 'all' }],
-  ['select-text', { 'user-select': 'text' }],
-  ['select-none', { 'user-select': 'none' }],
-]
+  ["select-auto", { "user-select": "auto" }],
+  ["select-all", { "user-select": "all" }],
+  ["select-text", { "user-select": "text" }],
+  ["select-none", { "user-select": "none" }],
+];
 
 export const whitespaces: Rule[] = [
-  [/^(?:whitespace|ws)-(normal|nowrap|pre|pre-line|pre-wrap)$/, ([, v]) => ({ 'white-space': v })],
-]
+  [
+    /^(?:whitespace|ws)-(normal|nowrap|pre|pre-line|pre-wrap)$/,
+    ([, v]) => ({ "white-space": v }),
+  ],
+];
 
 export const contents: Rule[] = [
-  ['content-empty', { content: '""' }],
-  ['content-none', { content: '""' }],
-]
+  ["content-empty", { content: '""' }],
+  ["content-none", { content: '""' }],
+];
 
 export const breaks: Rule[] = [
-  ['break-normal', { 'overflow-wrap': 'normal', 'word-break': 'normal' }],
-  ['break-words', { 'overflow-wrap': 'break-word' }],
-  ['break-all', { 'word-break': 'break-all' }],
-]
+  ["break-normal", { "overflow-wrap": "normal", "word-break": "normal" }],
+  ["break-words", { "overflow-wrap": "break-word" }],
+  ["break-all", { "word-break": "break-all" }],
+];
 
 export const textOverflows: Rule[] = [
-  ['truncate', { 'overflow': 'hidden', 'text-overflow': 'ellipsis', 'white-space': 'nowrap' }],
-  ['text-ellipsis', { 'text-overflow': 'ellipsis' }],
-  ['text-clip', { 'text-overflow': 'clip' }],
-]
+  [
+    "truncate",
+    {
+      overflow: "hidden",
+      "text-overflow": "ellipsis",
+      "white-space": "nowrap",
+    },
+  ],
+  ["text-ellipsis", { "text-overflow": "ellipsis" }],
+  ["text-clip", { "text-overflow": "clip" }],
+];
 
 export const textTransforms: Rule[] = [
-  ['case-upper', { 'text-transform': 'uppercase' }],
-  ['case-lower', { 'text-transform': 'lowercase' }],
-  ['case-capital', { 'text-transform': 'capitalize' }],
-  ['case-normal', { 'text-transform': 'none' }],
-]
+  ["case-upper", { "text-transform": "uppercase" }],
+  ["case-lower", { "text-transform": "lowercase" }],
+  ["case-capital", { "text-transform": "capitalize" }],
+  ["case-normal", { "text-transform": "none" }],
+];
 
 export const fontStyles: Rule[] = [
-  ['italic', { 'font-style': 'italic' }],
-  ['not-italic', { 'font-style': 'normal' }],
-]
+  ["italic", { "font-style": "italic" }],
+  ["not-italic", { "font-style": "normal" }],
+];
 
 export const fontSmoothings: Rule[] = [
-  ['antialiased', {
-    '-webkit-font-smoothing': 'antialiased',
-    '-moz-osx-font-smoothing': 'grayscale',
-    'font-smoothing': 'grayscale',
-  }],
-  ['subpixel-antialiased', {
-    '-webkit-font-smoothing': 'auto',
-    '-moz-osx-font-smoothing': 'auto',
-    'font-smoothing': 'auto',
-  }],
-]
+  [
+    "antialiased",
+    {
+      "-webkit-font-smoothing": "antialiased",
+      "-moz-osx-font-smoothing": "grayscale",
+      "font-smoothing": "grayscale",
+    },
+  ],
+  [
+    "subpixel-antialiased",
+    {
+      "-webkit-font-smoothing": "auto",
+      "-moz-osx-font-smoothing": "auto",
+      "font-smoothing": "auto",
+    },
+  ],
+];
+
+export const objectPositions: Rule[] = [
+  // object fit
+  ["object-cover", { "object-fit": "cover" }],
+  ["object-contain", { "object-fit": "contain" }],
+  ["object-fill", { "object-fit": "fill" }],
+  ["object-scale-down", { "object-fit": "scale-down" }],
+  ["object-none", { "object-fit": "none" }],
+
+  // object position
+  [/^object-([-\w]+)$/, ([, s]) => ({ "object-position": positionMap[s] })],
+];
