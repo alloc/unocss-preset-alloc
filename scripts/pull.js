b764ac5e541c8bb0b2430a3959d60d670f238c16
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-07-26 19:18:01 -0400

    chore(pull): apply patches to self, not upstream
    …and track the HEAD commit of previous update.

--- scripts/pull.js
+++ scripts/pull.js
@@ -2,7 +2,7 @@ const { crawl } = require('recrawl')
 const { clear, success } = require('misty')
 const { startTask } = require('misty/task')
 const { readFile, access, readdir, writeFile, unlink } = require('fs/promises')
-const { resolve, join } = require('path')
+const { resolve, join, relative } = require('path')
 const exec = require('@cush/exec')
 const prettier = require('prettier')
 const { gray, green, red } = require('kleur')
@@ -14,6 +14,10 @@ main()
 async function main() {
   let task = startTask('Downloading upstream')
 
+  const prevHead = (await exists('upstream.txt'))
+    ? await readFile('upstream.txt', 'utf8')
+    : null
+
   const upstreamDir = 'upstream'
   if (await exists(upstreamDir)) {
     await exec('git fetch origin main', { cwd: upstreamDir })
@@ -24,21 +28,18 @@ async function main() {
     ])
   }
 
-  task.finish()
-  task = startTask('Patching upstream')
-
-  const upstreamPkgRoot = 'packages/preset-mini'
-
-  const patches = await readdir('patches')
-  for (let patch of patches) {
-    patch = resolve('patches', patch)
-    await exec(
-      `git am --ignore-whitespace --directory=${upstreamPkgRoot} ${patch}`,
-      { cwd: upstreamDir }
+  const head = await exec('git rev-parse HEAD', { cwd: upstreamDir })
+  if (head !== prevHead) {
+    await writeFile('upstream.txt', head)
+  } else {
+    task.finish()
+    return success(
+      'No changes required?\n' +
+        gray('  You may want to delete ./upstream.txt if patches have changed.')
     )
   }
 
-  task.finish(`${patches.length} patches applied.`)
+  task.finish()
   task = startTask('Updating files')
 
   const readText = file => readFile(file, 'utf8')
@@ -46,56 +47,93 @@ async function main() {
     assert: { type: 'json' },
   })
 
+  const upstreamPkgRoot = 'packages/preset-mini'
+  const upstreamSrcDir = join(upstreamPkgRoot, 'src')
   const roots = {
     ours: 'src',
-    theirs: resolve(upstreamDir, upstreamPkgRoot, 'src'),
+    theirs: resolve(upstreamDir, upstreamSrcDir),
   }
 
-  const globs = [
-    '/rules/**.ts',
-    '/utils/**.ts',
-    '/theme/**.ts',
-    '/variants/**.ts',
-  ]
+  const dirs = ['rules', 'utils', 'theme', 'variants']
+  const crawlGlobs = dirs.map(dir => `/${dir}/**.ts`)
 
-  const upstreamFiles = await crawl(roots.theirs, {
-    only: globs,
-  })
-
-  for (let file of upstreamFiles) {
-    const upstreamFile = join(roots.theirs, file)
-    const newCode = prettier.format(await readText(upstreamFile), {
-      parser: 'typescript',
-      ...prettierConfig.default,
+  // Use git to get the list of files that have changed
+  // since the `head` commit.
+  let changedFiles
+  if (prevHead) {
+    const stdout = await exec(
+      `git diff --name-only ${prevHead}..${head} --`,
+      dirs.map(dir => join(upstreamSrcDir, dir)),
+      { cwd: upstreamDir }
+    )
+    changedFiles = stdout
+      .split('\n')
+      .map(file => relative(upstreamSrcDir, file))
+      .filter(file => file.endsWith('.ts'))
+  } else {
+    changedFiles = await crawl(roots.theirs, {
+      only: crawlGlobs,
     })
+  }
 
-    let oldCode
+  const deletedFiles = new Set()
+
+  let theirCode, ourCode
+  for (let file of changedFiles) {
+    const theirFile = join(roots.theirs, file)
+    if (await exists(theirFile)) {
+      theirCode = prettier.format(await readText(theirFile), {
+        parser: 'typescript',
+        ...prettierConfig.default,
+      })
+    } else {
+      deletedFiles.add(file)
+      continue
+    }
+
     try {
       file = join(roots.ours, file)
-      oldCode = await readText(file)
-    } catch {}
+      ourCode = await readText(file)
+    } catch {
+      ourCode = ''
+    }
 
-    if (newCode !== oldCode) {
-      await writeFile(file, newCode)
-      success(oldCode ? file : green(file))
+    if (theirCode !== ourCode) {
+      await writeFile(file, theirCode)
+      success(ourCode ? file : green(file))
     } else {
       success(gray(file))
     }
   }
 
-  const oldFiles = await crawl(roots.ours, {
-    only: globs,
+  const ourFiles = await crawl(roots.ours, {
+    only: crawlGlobs,
   })
 
-  for (let file of oldFiles) {
-    if (!upstreamFiles.includes(file)) {
-      file = join(roots.ours, file)
+  for (let file of ourFiles) {
+    file = join(roots.ours, file)
+    if (deletedFiles.has(file)) {
       await unlink(file)
       success(red(file))
     }
   }
 
   task.finish()
+  task = startTask('Applying patches')
+
+  const patches = await readdir('patches')
+  for (const name of patches) {
+    const file = resolve('patches', name)
+    try {
+      await exec(`git apply --ignore-whitespace ${file}`)
+    } catch (e) {
+      console.log('\n' + red(`Patch failed: "${name}"`))
+      console.error(e)
+      process.exit(1)
+    }
+  }
+
+  task.finish(`${patches.length} patches applied.`)
 }
 
 async function exists(file) {
