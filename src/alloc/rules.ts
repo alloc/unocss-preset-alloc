38410aa3c0350178972e7384787fd694ac3af92f
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-07-26 15:16:15 -0400

    feat: add `zoom-` for em-based scaling

--- src/alloc/rules.ts
+++ src/alloc/rules.ts
@@ -1,9 +1,10 @@
-import { Rule } from '@unocss/core'
+import { Rule, toArray } from '@unocss/core'
+import { Theme } from '../../theme'
 import { h } from '../utils'
 import { positionMap } from '../utils'
 import { filters } from './rules/filters'
 
-export const rules: Rule[] = [
+export const rules: Rule<Theme>[] = [
   ...filters,
 
   // background gradients
@@ -23,4 +24,22 @@ export const rules: Rule[] = [
 
   // object position
   [/^object-([-\w]+)$/, ([, s]) => ({ 'object-position': positionMap[s] })],
+
+  // em-based scaling
+  [
+    /^zoom-(.+)$/,
+    ([, s = 'base'], { theme }) => {
+      const themed = toArray(theme.fontSize?.[s])
+      if (themed?.[0]) {
+        const [size, height = '1'] = themed
+        return {
+          'font-size': size,
+          'line-height': height,
+        }
+      }
+
+      return { 'font-size': h.bracketOfLength.rem(s) }
+    },
+    { autocomplete: 'zoom-$fontSize' },
+  ],
 ]
