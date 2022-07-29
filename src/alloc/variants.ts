4bf18f0b1437dfd93ea2509c5a0167694eac58ef
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-07-28 20:40:27 -0400

    feat: add `zoom-` variant
    When using the zoom variant, all *direct* child elements have their font-size
    set to the provided `em` value.
    
    Make sure no children are `span` elements or have their own font-size set!

--- src/alloc/variants.ts
+++ src/alloc/variants.ts
@@ -2,7 +2,24 @@ import { variantMatcher } from '../utils'
 import { Variant } from '@unocss/core'
 
 export const variants: Variant[] = [
+  zoomVariant(),
   variantMatcher('span', input => ({
     selector: `${input.selector} span`,
   })),
 ]
+
+function zoomVariant(): Variant {
+  return {
+    name: 'zoom',
+    match(input: string) {
+      if (input.startsWith('zoom-')) {
+        return {
+          matcher: input,
+          selector(input) {
+            return `${input} > *`
+          },
+        }
+      }
+    },
+  }
+}
